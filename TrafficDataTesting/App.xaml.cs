04c33421378747a4c13b6d30fd166d58698216c8
GitRKhanHub <GitRKhanHub@github.com>
2021-08-30 10:46:01 +0500

    Start WPF proj

---
+++ TrafficDataTesting/App.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Documents;
+
+namespace TrafficDataTesting
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static bool runner0 = true; //условие для цикла 0-го столбца генерации
+        public static bool runner1 = true; //условие для цикла 1-го столбца генерации
+        public static bool runner2 = true; //условие для цикла 2-го столбца генерации
+        public static int period = 100; //временной период в мс
+
+        public static int FourSignNumber_Generator() //генератор случ. 4-хзнач. положит. цел. числа
+        {
+            {
+                Random rnd = new Random();
+                int temp = rnd.Next(1000, 10000);
+
+                return temp;
+            }
+        }
+
+        public static bool isSimpleNumber(int num) //проверка на простоту числа
+        {
+            bool res = true;
+            for (int i = 2; i < num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    res = false;
+                    break;
+
+                }
+
+            }
+            return res;
+        }
+
+        //public static async void Cycle_Number_GeneratorAsync(TextBlock tb, bool runner) //генератор с периодом, заполняющий окно
+        //{
+        //    int num;
+
+
+        //    while (runner)
+        //    {
+        //        num = App.FourSignNumber_Generator();
+        //        if (App.isSimpleNumber(num))
+        //        {
+        //            tb.Inlines.Add(new Run(" " + num) { Foreground = Brushes.Red });
+        //        }
+        //        else
+        //            tb.Inlines.Add(new Run(" " + num));
+        //        await Task.Delay(2000);
+
+        //    }
+        //} 
+    }
+}
---
+++ TrafficDataTesting/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Threading;
+
+namespace TrafficDataTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //переменные и методы находятся в App.xaml.cs
+
+        private async void startButton_0_Click(object sender, RoutedEventArgs e)
+        {
+            
+            App.runner0 = true;
+            int num;
+
+           
+            while (App.runner0)
+            {
+                num = App.FourSignNumber_Generator();
+                if (App.isSimpleNumber(num))
+                {
+                    TextBlock_0.Inlines.Add(new Run(" " + num) { Foreground = Brushes.Red });
+                }
+                else
+                    TextBlock_0.Inlines.Add(new Run(" " + num));
+                await Task.Delay(App.period);
+
+            }
+
+        }
+
+        private void stopButton_0_Click(object sender, RoutedEventArgs e)
+        {
+
+            App.runner0 = false;
+        }
+
+        private async void startButton_1_Click(object sender, RoutedEventArgs e)
+        {
+            App.runner1 = true;
+            int num;
+
+
+            while (App.runner1)
+            {
+                num = App.FourSignNumber_Generator();
+                if (App.isSimpleNumber(num))
+                {
+                    TextBlock_1.Inlines.Add(new Run(" " + num) { Foreground = Brushes.Red });
+                }
+                else
+                    TextBlock_1.Inlines.Add(new Run(" " + num));
+                await Task.Delay(App.period);
+
+            }
+
+        }
+
+        private void stopButton_1_Click(object sender, RoutedEventArgs e)
+        {
+            App.runner1 = false;
+        }
+
+        private async void startButton_2_Click(object sender, RoutedEventArgs e)
+        {
+            App.runner2 = true;
+            int num;
+
+
+            while (App.runner2)
+            {
+                num = App.FourSignNumber_Generator();
+                if (App.isSimpleNumber(num))
+                {
+                    TextBlock_2.Inlines.Add(new Run(" " + num) { Foreground = Brushes.Red });
+                }
+                else
+                    TextBlock_2.Inlines.Add(new Run(" " + num));
+                await Task.Delay(App.period);
+
+            }
+        }
+
+        private void stopButton_2_Click(object sender, RoutedEventArgs e)
+        {
+            App.runner2 = false;
+        }
+    }
+}
